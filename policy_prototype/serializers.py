676b91ee0a47bf3738d3b9af6613587fa41624a0
jakobpederson <jakob.pederson+github@gmail.com>
2017-09-22 15:05:26 -0500

    working as expected?

--- policy_prototype/serializers.py
+++ policy_prototype/serializers.py
@@ -4,31 +4,28 @@ from policy_prototype.models import Policy, Coverage
 
 
 class PolicySerializer(serializers.HyperlinkedModelSerializer):
-    # coverages = relations.ResourceRelatedField(read_only=True, many=True)
     coverages = serializers.SerializerMethodField('get_link')
 
     def get_link(self, obj):
-        print(obj.coverages.all())
         url = "http://localhost:8000/api/coverages/{}/"
         new_list = [url.format(x.pk) for x in obj.coverages.all()]
         return new_list
 
-    # coverages = relations.HyperlinkedRelatedField(
-    #     read_only=True,
-    #     many=True,
-    #     view_name='coverages',
-    #     lookup_field='',
-    # )
-
     class Meta:
         model = Policy
-        fields = ('policy_number', 'coverages') # , 'url')
-        # extra_kwargs = {
-        #     'url': {'view_name': 'coverage-detail'}
-        # }
+        fields = ('policy_number', 'coverages')
 
 
 class CoverageSerializer(serializers.HyperlinkedModelSerializer):
+
+    policy = serializers.SerializerMethodField('get_link')
+
+    def get_link(self, obj):
+        url = "http://localhost:8000/api/policies/{}/"
+        new_list = url.format(obj.policy.id)
+        return new_list
+
+
 
     class Meta:
         model = Coverage
